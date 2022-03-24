9928e1f9cba2c039d7a83ed85319375c73a385bc
Alexander Petrov <i453628@fhict.nl>
2022-03-24 15:28:05 +0100

    Scheduling part

---
+++ MediaBazzarApplication/MediaBazzarApplication/Logic/ShiftType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazzarApplication.Logic
+{
+    public enum ShiftType { 
+    MondayMorning, MondayAfternoon, MondayEvening, 
+    TuesdayMorning, TuesdayAfternoon, TuesdayEvening,
+    WednesdayMorning, WednesdayAfternoon, WednesdayEvening,
+    ThursdayMorning, ThursdayAfternoon, ThursdayEvening,
+    FridayMorning, FridayAfternoon, FridayEvening,
+    SaturdayMorning, SaturdayAfternoon, SaturdayEvening,
+    SundayMorning, SundayAfternoon, SundayEvening
+    }
+}
